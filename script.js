7bd090c476529492b3baa9b3d88e79781e994c05
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-25 19:42:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ let incrementElement = document.getElementById("incrementBtn");
 
 
 function updateDOM() {
-	
 	counterElement.textContent = count;
-	
 }
+updateDOM()
 
 incrementElement.addEventListener("click",()=>{
 	alert(count)
