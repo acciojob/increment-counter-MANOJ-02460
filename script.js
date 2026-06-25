7256a2375dd957e7d4edfe510292de5e02e9dff3
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-25 19:11:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let count = 0;
+
+let counterElement = document.getElementById("counter");
+
+
+function incrementBtn() {
+	alert(count)
+	counterElement.textContent = count++;	
+}
+
+
+
+
+
+
+
+
